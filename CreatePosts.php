1f88b27e42448f404ba2cbd96bf39e8aa8117829
Anissa Khan <anissa.khan@gmail.com>
2019-10-25 17:59:23 -0500

    Exercises 3 and 4 done

--- CreatePosts.php
+++ CreatePosts.php
@@ -7,53 +7,53 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
-$query = "SELECT * FROM Posts ORDER by post_id";
-
-if ($result = $mysqli->query($query)) {
-
-    /* fetch associative array */
-    while ($row = $result->fetch_assoc()) {
-        printf ("%s (%s)\n", $row["post_id"], $row["content"]);
-    }
-
-    /* free result set */
-    $result->free();
-}
-
-/* close connection */
-$mysqli->close();
-?>
-
-//NEW
-
-<?php
-$mysqli = new mysqli("mysql.eecs.ku.edu", "anissakhan", "eo9eVey9", "anissakhan");
-
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-
-//$query = "SELECT * FROM Posts ORDER by post_id";
 $userName = $_POST["username"];
-$query = "INSERT INTO Users (user_id) VALUES ('$userName')"; 
+$content = $_POST["content"];
+$existingUser = 0;
 
+//query1: adds post to table; query2: looks at the User table to see if user exists
+//$query1 = "INSERT INTO Posts (author_id) VALUES ('".$userName."')";
+$query1 = "INSERT INTO Posts (author_id, content) VALUES ('".$userName."','".$content."')";
+$query2 = "SELECT user_id FROM Users";
 
-if ($result = $mysqli->query($query)) {
+//check if either username or post are empty
+if($userName=="" || $content=="") {
+         echo "You must enter both a Username and a Post.<br>";
+         exit();
+ }
 
-    /* fetch associative array */
+//check if the username is in the Users table. If it is not, enters the else statement and exits the program.
+//if it is in the table, does nothing
+if ($result = $mysqli->query($query2)) {
+    /* fetch associative array; while fetch_assoc is not null: keeps bringing stuff in */
     while ($row = $result->fetch_assoc()) {
         if($row["user_id"]==$userName)
         {
-            printf ("Username already exists.");
-        }
-        //printf ("%s %s Name:(%s)\n", $row["post_id"], $row["content"], $row["author_id"]);
+		//does nothing. only exits if the username is does not exist.
+		$existingUser = 1;
+	}
     }
 
-    /* free result set */
-    $result->free();
+    if($existingUser==0)
+    {	
+    		echo "Username ".$userName." does not exist. Enter an existing username.<br>";
+                exit();
+    }
+
+    $result->free(); 
 }
+
+
+//do query1: add the post to the table
+$mysqli->query($query1);
+echo "Your post has been successfully added.<br>";
+
+//$query3 = "SELECT LAST_INSERT_ID()";
+
+//if($result = $mysqli->query($query3)){
+//	echo "post_id: ".$row."<br>";
+//}
+//$result->free();
 
 /* close connection */
 $mysqli->close();
